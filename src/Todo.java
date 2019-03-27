25cb9e1bb3c2df2284eeb452493edcce9309c454
Brian Carlton <brian.carlton@ou.edu>
2019-03-26 23:15:09 -0500

    Release 1.1 add Todo.java

---
+++ src/Todo.java
@@ -0,0 +1,46 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-26
+OU ID_______________: 113109413
+OU EMAIL____________: brian.carlton@ou.edu
+
+Tasks:
+1. implement BoardGame
+2. implement BoardGameTest
+3. implement Color
+4. implement ColorTest
+5. implement GamePiece
+6. implement GamePieceTest
+7. implement GamePieceAppearance
+8. implement GamePieceAppearanceTest
+9. implement Location
+10.implement Shape
+11.implement ShapeTest
+
+Estimated Time (minutes):
+1. 15
+2. 15
+3. 5
+4. 5
+5. 7
+6. 10
+7. 8
+8. 7
+9. 3
+10. 5
+11. 7
+
+Actual Time (minutes):
+1. 15
+2. 16
+3. 7
+4. 7
+5. 7
+6. 10
+7. 8
+8. 7
+9. 3
+10. 4
+11. 7
+
+*/}
